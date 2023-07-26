7d5a18af8cd870e93161df4ecd75cf93910516b6
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2023-07-26 23:40:16 +0530

    more cosmetic changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
 // Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../css/header.css';
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header>
       <nav className="navbar">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/submit">Submit a Project</Link>
-          </li>
-        </ul>
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          Home
+        </Link>
+        <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>
+          Projects
+        </Link>
+        <Link to="/submit" className={location.pathname === '/submit' ? 'active' : ''}>
+          Submit a Project
+        </Link>
       </nav>
     </header>
   );
